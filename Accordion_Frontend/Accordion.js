ef114eef3d7e34392a7dab66c8de72c33faf5793
addygupta21 <kaaditya1008@gmail.com>
2025-05-25 13:33:59 +0530

    add: accordion coding files

--- Accordion_Frontend/Accordion.js
+++ Accordion_Frontend/Accordion.js
@@ -1,3 +1,23 @@
-const Accordion = () => {
-    
+import React, { useState } from "react";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import './styles.css'
+
+function Accordion({ items }) {
+    const [openIndex, setOpenIndex] = useState(null);
+    const handleToggle = (index) => {
+        setOpenIndex(openIndex == index ? null : index);
+    }
+    return !items || (items.length == 0) ? "No items available" : (
+        <div className="accordion">
+            {items.map((item, index) => {
+                return <div key={index} className="accordion-item">
+                    <button className="accordion-title" onClick={() => handleToggle(index)}> {item.title}
+                        {openIndex == index ? <FaChevronUp className="right" /> : <FaChevronDown />} </button>
+                    {openIndex == index && <div className="accordion-content" > {item.content}</div>}
+                </div>
+            })}
+        </div>
+    );
 }
+
+export default Accordion;
